b5a76530113191383149b06c1c1825fc2410b7b0
madhavchy514 <madhavchy.p@gmail.com>
2026-04-30 21:41:15 +0600

    Creating new python project which describes video

---
+++ python/ollama_video_describer/main.py
@@ -0,0 +1,49 @@
+import ollama
+import cv2
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+def describe_local_video(file_path: str):
+    if not os.path.exists(file_path):
+        print(f"[error]: file not found: {file_path}")
+        return
+
+    print(f"[process]: analyzing: {os.path.basename(file_path)}")
+
+    cap = cv2.VideoCapture(file_path)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    cap.set(cv2.CAP_PROP_POS_FRAMES, int(total_frames * 0.4))
+    success, frame = cap.read()
+    
+    if not success:
+        print("[error]: could not extract frame")
+        cap.release()
+        return
+
+    img_name = "frame.jpg"
+    cv2.imwrite(img_name, frame)
+    cap.release()
+
+    try:
+        response = ollama.chat(
+            model=os.getenv('MODEL'),
+            messages=[{
+                'role': 'user',
+                'content': os.getenv('PROMPT'),
+                'images': [img_name] 
+            }]
+        )
+
+        print(f'[response]: {response['message']['content']}')
+
+    except Exception as e:
+        print(f"[error]: {e}")
+
+    if os.path.exists(img_name):
+        os.remove(img_name)
+
+if __name__ == "__main__":
+    describe_local_video(os.getenv('FILE'))
